f8e0c8f3e6634a19fb3a82da1e9493764a487c6e
Quaterno <atom112358@gmail.com>
2015-02-11 18:44:56 +0000

    Fixing git problems

--- GroupSketching/GroupSketching/SketchHandler.cpp
+++ GroupSketching/GroupSketching/SketchHandler.cpp
@@ -12,11 +12,7 @@ SketchHandler::~SketchHandler(void) {
 }
 
 Formation* SketchHandler::processFormation(vector<glm::vec3> stroke) {
-<<<<<<< HEAD
-	// Distance-relative resampling
-=======
 	// Distance-variable resampling
->>>>>>> 553ad2f0cf5c5bc394bb7712041db4652e25c61f
 	vector<glm::vec3> newStroke;
 	newStroke.push_back(stroke[0]);
 	for(vector<glm::vec3>::size_type i = 1; i < stroke.size(); i++) {
@@ -25,26 +21,6 @@ Formation* SketchHandler::processFormation(vector<glm::vec3> stroke) {
 			newStroke.push_back(stroke[i]);
 		}
 	}
-<<<<<<< HEAD
-
-	bool lineCross = false;
-	int firstLine = 0;
-	int secondLine = 0;
-	glm::vec3 intersect = glm::vec3(0, 0, 0);
-	for(vector<glm::vec3>::size_type j = 1; j < newStroke.size(); j++) {
-		for(vector<glm::vec3>::size_type k = (j+2); k < newStroke.size(); k++) {
-			float m_1, m_2, c_1, c_2;
-			m_1 = ((newStroke[j].z - newStroke[j-1].z) / (newStroke[j].x - newStroke[j-1].x));
-			m_2 = ((newStroke[k].z - newStroke[k-1].z) / (newStroke[k].x - newStroke[k-1].x));
-
-			c_1 = newStroke[j].z - (m_1 * newStroke[j].x);
-			c_2 = newStroke[k].z - (m_2 * newStroke[k].x);
-
-			intersect.x = (c_2 - c_1) / (m_1 - m_2);
-			intersect.z = ((m_1 * c_2) - (m_2 * c_1)) / (m_1 - m_2);
-			intersect.y = newStroke[j].y;
-
-=======
 	
 	// Line crossing handler
 	bool lineCross = false;
@@ -65,7 +41,6 @@ Formation* SketchHandler::processFormation(vector<glm::vec3> stroke) {
 			intersect.y = newStroke[j].y;
 
 			// Check if intesect point is on the first line segment
->>>>>>> 553ad2f0cf5c5bc394bb7712041db4652e25c61f
 			bool intersectOnFirstLine = false;
 			if (intersect.x <= newStroke[j].x) {
 				if (intersect.x >= newStroke[j-1].x) {
@@ -77,11 +52,7 @@ Formation* SketchHandler::processFormation(vector<glm::vec3> stroke) {
 					intersectOnFirstLine = true;
 				}
 			}
-
-<<<<<<< HEAD
-=======
 			// If it is then check the second line segment before flagging crossing lines
->>>>>>> 553ad2f0cf5c5bc394bb7712041db4652e25c61f
 			if (intersectOnFirstLine) {
 				if (intersect.x <= newStroke[k].x) {
 					if (intersect.x >= newStroke[k-1].x) {
@@ -105,12 +76,7 @@ Formation* SketchHandler::processFormation(vector<glm::vec3> stroke) {
 
 	if (lineCross) {
 		newStroke.erase(newStroke.begin()+secondLine, newStroke.end());
-<<<<<<< HEAD
 		newStroke.erase(newStroke.begin(), newStroke.begin()+firstLine);
-=======
-		newStroke.erase(newStroke.begin(), newStroke.begin()+(firstLine-1));
-		newStroke[0] = intersect;
->>>>>>> 553ad2f0cf5c5bc394bb7712041db4652e25c61f
 	}
 
 	Formation* formation = new Formation(newStroke);
