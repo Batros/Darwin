aa76c493d05455bd3e2b33da07abe580002fc2f2
Quaterno <atom112358@gmail.com>
2015-02-11 18:34:37 +0000

    Added line crossing handling

--- GroupSketching/GroupSketching/SketchHandler.cpp
+++ GroupSketching/GroupSketching/SketchHandler.cpp
@@ -12,6 +12,7 @@ SketchHandler::~SketchHandler(void) {
 }
 
 Formation* SketchHandler::processFormation(vector<glm::vec3> stroke) {
+	// Distance-relative resampling
 	vector<glm::vec3> newStroke;
 	newStroke.push_back(stroke[0]);
 	for(vector<glm::vec3>::size_type i = 1; i < stroke.size(); i++) {
@@ -20,6 +21,62 @@ Formation* SketchHandler::processFormation(vector<glm::vec3> stroke) {
 			newStroke.push_back(stroke[i]);
 		}
 	}
+
+	bool lineCross = false;
+	int firstLine = 0;
+	int secondLine = 0;
+	glm::vec3 intersect = glm::vec3(0, 0, 0);
+	for(vector<glm::vec3>::size_type j = 1; j < newStroke.size(); j++) {
+		for(vector<glm::vec3>::size_type k = (j+2); k < newStroke.size(); k++) {
+			float m_1, m_2, c_1, c_2;
+			m_1 = ((newStroke[j].z - newStroke[j-1].z) / (newStroke[j].x - newStroke[j-1].x));
+			m_2 = ((newStroke[k].z - newStroke[k-1].z) / (newStroke[k].x - newStroke[k-1].x));
+
+			c_1 = newStroke[j].z - (m_1 * newStroke[j].x);
+			c_2 = newStroke[k].z - (m_2 * newStroke[k].x);
+
+			intersect.x = (c_2 - c_1) / (m_1 - m_2);
+			intersect.z = ((m_1 * c_2) - (m_2 * c_1)) / (m_1 - m_2);
+			intersect.y = newStroke[j].y;
+
+			bool intersectOnFirstLine = false;
+			if (intersect.x <= newStroke[j].x) {
+				if (intersect.x >= newStroke[j-1].x) {
+					intersectOnFirstLine = true;
+				}
+			}
+			else {
+				if (intersect.x <= newStroke[j-1].x) {
+					intersectOnFirstLine = true;
+				}
+			}
+
+			if (intersectOnFirstLine) {
+				if (intersect.x <= newStroke[k].x) {
+					if (intersect.x >= newStroke[k-1].x) {
+						lineCross = true;
+						firstLine = j;
+						secondLine = k;
+						break;
+					}
+				}
+				else {
+					if (intersect.x <= newStroke[k-1].x) {
+						lineCross = true;
+						firstLine = j;
+						secondLine = k;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	if (lineCross) {
+		newStroke.erase(newStroke.begin()+secondLine, newStroke.end());
+		newStroke.erase(newStroke.begin(), newStroke.begin()+firstLine);
+	}
+
 	Formation* formation = new Formation(newStroke);
 	return formation;
 
